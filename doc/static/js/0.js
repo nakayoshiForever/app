f6e71e3f5f204a95e287a19147754b6c941a5750
Pocchi <pocchi13.tr@gmail.com>
2018-02-25 11:00:24 +0900

    build

--- doc/static/js/0.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([0],{"H/op":function(o,n,F){var c=F("YdFs");"string"==typeof c&&(c=[[o.i,c,""]]),c.locals&&(o.exports=c.locals);F("rjj0")("4200e6d4",c,!0)},YdFs:function(o,n,F){(o.exports=F("FZ+f")(!1)).push([o.i,":root {\n  --contents-color: #FFFFFF;\n  --main-color: #4E4747;\n  --accent-color: #CDEF23;\n  --base-color: #B3C0D1;\n  --white-color: #FFFFFF; }\n",""])}});
--- docs/dist/static/js/1.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([1],{"hv/O":function(n,c){}});
